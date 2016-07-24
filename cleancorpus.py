78a8e6c74f825bd55b547715713958d70187f259
lappe <lappe@last.cl.uni-heidelberg.de>
2016-07-24 12:22:48 +0200

    wrote a program for cleaning corpus entrys and added the resulting new mrls

---
+++ cleancorpus.py
@@ -0,0 +1,20 @@
+import os
+
+inputfolder = "./Endcorpus"
+
+is_mrlfile = lambda name: True if name.endswith(".mrl") else False
+
+for dirname,subdirs,filelist in os.walk(inputfolder):
+	for fname in filelist:
+		if not is_mrlfile(fname):
+			continue
+		with open(dirname+"/"+fname) as f:
+			with open(dirname+"/2"+fname,"w+") as out:
+				for line in f:
+					print (line)
+					line = line.replace("  "," ")
+					line = line.replace(" "," ")
+					line = line.replace(", ",",")
+					line = line.replace("‘","'")
+					line = line.replace("’","'")
+					out.write(line)
