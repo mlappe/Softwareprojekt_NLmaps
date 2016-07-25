01d1808d78e931833f89a952c5f47fb507109fb4
Killer1337 <yilmazoz95@gmail.com>
2016-07-25 13:12:42 +0200

    Add files via upload

--- Linearizer/Delinearizer.py
+++ Linearizer/Delinearizer.py
@@ -1,7 +1,7 @@
 # Noch nicht ganz fertig.... Aufruf: fileWriter(LinearisiertesFile,ZielFile(Delinearisiert))
 
 def delinearizer(LMRL):
-    inpt = LMRL.split(" ")
+    inpt = filter(None,LMRL.split(" "))
     first_strng = "$"
     stack = []
     ques = []
@@ -16,7 +16,10 @@ def delinearizer(LMRL):
                     stri = stri + dic[b]
                 stri = "'<nom>"+stri+"</nom>'"
                 first_strng = first_strng.replace("$",stri,1)
-            else:    
+                ques.append(a)
+                stack.append("0")
+            else:
+                
                 que,no = a.split("@")
                 stack.append(no[0])
                 ques.append(que)
@@ -46,7 +49,7 @@ def fileReader(checkfile,goldfile):
     with open(goldfile) as gold:
         gold_lst = gold.readlines()
     for index,a in enumerate(check_lst):
-        if delinearizer(a).strip() == gold_lst[index].strip():
+        if delinearizer(a).strip() == gold_lst[index].strip().replace("> ",">").replace(" <","<").replace("   "," "):
             print (True)
         else:
             print (index,False)
@@ -61,8 +64,8 @@ def fileWriter(inpt,outpt):
             file.write(delinearizer(a)+"\n")
     
         
-
-#fileReader("MRL_EN_TRAIN_YANG_linearizedTEST2.train.LMRL","Downloads/test.mrl")    
-#print (fileReader2("Schreibtisch/nlmaps/nlmaps.train.mrl"))
-fileWriter("MRL_EN_TRAIN_YANG_linearizedTEST3.train.LMRL","Schreibtisch/Delinearized3.txt")
-#print (delinearizer("query@3 area@1 keyval@2 name@0 fjbbhfmmcl nwr@1 keyval@2 name@0 Loreleyhafen@s qtype@1 findkey@1 seamark:harbour:category@0"))
+if __name__ == "__main__":
+    #fileReader("MRL_EN_TRAIN_YANG_linearizedTEST3.train.LMRL","Downloads/Softwareprojekt_NLmaps-master/Endcorpus/train.mrl")    
+    #print (fileReader2("Schreibtisch/nlmaps/nlmaps.train.mrl"))
+    #fileWriter("MRL_EN_TRAIN_YANG_linearizedTEST3.train.LMRL","Schreibtisch/Delinearized3.txt")
+    #print (delinearizer("query@3 area@1 keyval@2 name@0 fjbbbjdgbk  nwr@2 keyval@2 amenity@0 recycling@s keyval@2 recycling:glass@0 yes@s qtype@1 least@1 topx@1 1@0"))
