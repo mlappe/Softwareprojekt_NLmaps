bf0d7707e66c59cc8bbc2212f55c11a42798c492
lappe <lappe@cl.uni-heidelberg.de>
2016-07-30 22:09:30 +0200

    added flags to the script

--- nmt/runnmt.sh
+++ nmt/runnmt.sh
@@ -1,3 +1,3 @@
 python3 ./translate.py --maxSteps 1600 --size 256 --num_layers 3 --learning_rate 0.5
 python3 ./translate.py --decode --beam 100
-python2 ../Evaluation/evaluate.py ../nmt/nmtout.mrl ../Endcorpus/test.mrl
+python2 ../Evaluation/evaluator.py ../nmt/nmtout.mrl ../Endcorpus/test.mrl
