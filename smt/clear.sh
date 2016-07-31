b59e1089af3533cbf0430022bd5da40064bf49bd
Killer1337 <yilmazoz95@gmail.com>
2016-07-31 18:08:46 +0200

    Add files via upload

---
+++ smt/clear.sh
@@ -0,0 +1,9 @@
+rm -r lm
+rm -r mert-work
+rm -r train
+rm bestofList
+rm listfile
+rm mert.out
+rm outputOfTest.txt
+rm training.out
+rm -r __pycache__
--- smt/mosespipe.sh
+++ smt/mosespipe.sh
@@ -1,14 +1,18 @@
 # THIS IS A PIPELINE FOR TRAINING AND TESTING OF A STATISTICAL MACHINE TRANSLATION SYSTEM BASED ON MOSES
-# This pipeline needs to be in the folder where the output should come out(in this case progTest).
+# This pipeline needs to be in the folder where the output should come out(in this case progTest2), just like the select_from_nbest.py.
 # The corpora should be in a subfolder called 'corps' of that folder.
 # You need moses and all it's dependencies installed (look at http://www.statmt.org/moses/), and moses accessable from the root directory(~/mosesdecoder/...)
 # Also you should have GIZA++ with all it's necessary dependencies installed in ~/mosesdecoder/tools
-# Just change the paths below and start. Final output in Terminal should be a bleuscore.
+# Just change the paths below and start.
+# You can turn tuning on or off by setting TUNING=true or false. -> If false, Tune sets are not used. 
+# NOTE: MRL-FILES should already be linearized with the num-to-consonants version of Linearizer.
+# NOTE2: In general, Machine Translation training is non-convex. this means that there are multiple solutions and each time you run a full training job,
+# you will get different results.
 
 #Relative path to folder with Corpus and pipeline script:
-BASIC_PATH_RELATIVE="~/Desktop/swp/"
+BASIC_PATH_RELATIVE="~/Schreibtisch/progTest2/"
 #Absolute path to folder with Corpus and pipeline script:
-BASIC_PATH_ABSOLUTE="/home/david/Desktop/swp/"
+BASIC_PATH_ABSOLUTE="/home/ozan/Schreibtisch/progTest2/"
 
 TUNING=true
 
@@ -39,7 +43,6 @@ wait
 if [ $TUNING ]
 	then
 		nohup nice ~/mosesdecoder/scripts/training/mert-moses.pl $BASIC_PATH_ABSOLUTE$TUNE_INPUTLANG $BASIC_PATH_ABSOLUTE$TUNE_OUTPUTLANG ~/mosesdecoder/bin/moses train/model/moses.ini --mertdir ~/mosesdecoder/bin/ &> $BASIC_PATH_ABSOLUTE"mert.out" &
-50
 		wait
 		#add/remove parameters ("-n-best-list listfile" for n-best translations, "50 distinct" for 50 different translations
 		~/mosesdecoder/bin/moses -f mert-work/moses.ini -n-best-list listfile 50 distinct < $BASIC_PATH_ABSOLUTE$TEST_INPUTLANG > outputOfTest.txt
@@ -53,3 +56,6 @@ fi
 
 ~/mosesdecoder/scripts/generic/multi-bleu.perl $TEST_OUTPUTLANG < outputOfTest.txt 
 
+python3 select_from_nbest.py listfile bestofList
+
+#python3 evaluator.py bestofList $TEST_OUTPUTLANG
